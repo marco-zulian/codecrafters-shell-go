a90e5d9aaaecb3e57745c3aa327bf320e22f4984
Marco Zulian <codebymarco1@gmail.com>
2025-06-10 18:31:33 -0300

    implements echo

--- app/main.go
+++ app/main.go
@@ -15,10 +15,20 @@ func main() {
 		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		trimCommand := strings.TrimSpace(command)
 
-		exitRegex := regexp.MustCompile(`^exit (\d*)$`)
+		exitRegex := regexp.MustCompile(`^exit(\s\d*)?$`)
+		echoRegex := regexp.MustCompile(`^echo(\s.*)?$`)
 		switch {
 		case exitRegex.MatchString(trimCommand):
 			return
+		case echoRegex.MatchString(trimCommand):
+			match := echoRegex.FindStringSubmatch(trimCommand)
+			echoStr := match[1]
+
+			if len(echoStr) > 1 {
+				fmt.Println(match[1][1:])
+			} else {
+				fmt.Println()
+			}
 		default:
 			fmt.Println(command[:len(command)-1] + ": command not found")
 		}
