26123e0c77dd06821710838e034d05c30715f9f4
Marco Zulian <codebymarco1@gmail.com>
2025-06-10 18:48:23 -0300

    implements type

--- app/main.go
+++ app/main.go
@@ -17,6 +17,8 @@ func main() {
 
 		exitRegex := regexp.MustCompile(`^exit(\s\d*)?$`)
 		echoRegex := regexp.MustCompile(`^echo(\s.*)?$`)
+		typeRegex := regexp.MustCompile(`^type(\s.*)?$`)
+
 		switch {
 		case exitRegex.MatchString(trimCommand):
 			return
@@ -28,6 +30,17 @@ func main() {
 				fmt.Println(match[1][1:])
 			} else {
 				fmt.Println()
+			}
+		case typeRegex.MatchString(trimCommand):
+			commands := map[string]bool{"type": true, "echo": true, "exit": true}
+			match := typeRegex.FindStringSubmatch(trimCommand)
+
+			if len(match[1]) <= 1 {
+				fmt.Println("usage: type <command>")
+			} else if _, ok := commands[match[1][1:]]; ok {
+				fmt.Println(match[1] + " is a shell builtin")
+			} else {
+				fmt.Println(match[1] + ": not found")
 			}
 		default:
 			fmt.Println(command[:len(command)-1] + ": command not found")
